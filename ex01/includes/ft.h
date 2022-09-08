1bd52d771bdc42b9555b4756876fd44ceaede937
Kaigama's List <96994469+kaigama2@users.noreply.github.com>
2022-09-09 01:52:00 +0200

    Add files via upload

---
+++ ex01/includes/ft.h
@@ -0,0 +1,13 @@
+#ifndef ft_h
+#define FT_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void ft_putchar(char c);
+void ft_putstr(char *str);
+int ft_strcmp(char *s1, char *s2);
+int ft_strlen(char *str);
+void ft_swap(int *a, int *b);
+
+#endif
